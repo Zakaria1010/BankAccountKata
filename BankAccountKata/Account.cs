b9671f5d1474f9bd8e3454b3a747523562061ae1
ZakariaLaqibi <ZakariaLaqibi@INVFRA000854>
2023-06-05 12:32:00 +0200

    Ajoutez des fichiers projet.

---
+++ BankAccountKata/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountKata
+{
+    public class Account
+    {
+
+        private Transaction transaction;
+        private static double balance;
+        private IList<Transaction> transactions = new List<Transaction>();
+
+
+        public void Deposit(double amount, string date)
+        {
+            transaction = new Transaction(amount, date);
+            transactions.Add(transaction);
+        }
+
+        public void Withdraw(double amount, string date) 
+        {
+            transaction = new Transaction(-amount, date);
+            transactions.Add(transaction);
+        }
+        public void PrintStatement()
+        {
+            Console.WriteLine("DATE|AMOUNT|BALANCE");
+            foreach(var transaction in transactions)
+            {
+                balance += transaction.amount;
+                if(transaction.amount >= 0)
+                    Console.WriteLine($"{transaction.date}|{transaction.amount:F2}|{balance:F2}");
+                else
+                    Console.WriteLine($"{transaction.date}|{transaction.amount}|{balance:F2}");
+
+            }
+
+        }
+
+        class Transaction
+        {
+            public double amount;
+            public string date;
+            public double balance;
+            public Transaction(double amount, string date) 
+            { 
+                this.amount = amount;
+                this.date = date;
+            }
+        }
+    }
+}
---
+++ BankAccountKataTests/AccountTest.cs
@@ -0,0 +1,92 @@
+﻿using BankAccountKata;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountKataTests
+{
+    public class AccountTest
+    {
+
+        // print statement on console
+        private Account account;
+        public AccountTest() {
+            account = new Account();
+        }
+
+
+        [Fact]
+        public void Should_Print_Statement_Header()
+        {
+            // console shoule display
+            var recorder = new ConsoleRecorder();
+
+            using(recorder)
+            {
+                 account.PrintStatement();
+            }
+            
+            recorder.Display.Trim('\r', '\n').Should().Be("DATE|AMOUNT|BALANCE");
+        }
+
+        [Fact]
+        public void Should_Add_Deposit_Line_To_Statement()
+        {
+            account.Deposit(500, "01/04/2014");
+
+            var recorder = new ConsoleRecorder();
+
+            using (recorder)
+            {
+                account.PrintStatement();
+            }
+
+            recorder.Display.Trim('\r', '\n').Should().Be(
+                "DATE|AMOUNT|BALANCE\r\n01/04/2014|500,00|500,00"
+                );
+
+        }
+
+        [Fact]
+        public void Should_Add_WithDraw_Line_To_Statement()
+        {
+            account.Deposit(500, "01/04/2014");
+            account.Withdraw(100, "02/04/2014");
+           
+            var recorder = new ConsoleRecorder();
+
+            using (recorder)
+            {
+                account.PrintStatement();
+            }
+
+            recorder.Display.Trim('\r', '\n').Should().Be(
+                "DATE|AMOUNT|BALANCE\r\n" +
+                "01/04/2014|500,00|500,00\r\n" +
+                "02/04/2014|-100|400,00"
+                );
+        }
+    }
+
+    internal class ConsoleRecorder: IDisposable
+    {
+        private readonly TextWriter oldOutputStream;
+        private readonly StringWriter recorder;
+        public string Display => recorder.ToString();
+        
+        public ConsoleRecorder()
+        {
+            oldOutputStream = Console.Out;
+            recorder = new StringWriter();
+            Console.SetOut(recorder);
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(oldOutputStream);
+        }
+    }
+}
